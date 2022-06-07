7a8585815a18eda075daff03377ad72708d1ca83
Tyler <101354625+tylerarend@users.noreply.github.com>
2022-06-07 18:09:25 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -5,38 +5,32 @@ const PORT = 8000
 
 app.use(cors())
 
-const rappers = {
-    '21 savage': {
-        'age': 29,
-        'birthName': 'Shéyaa Bin Abraham-Joseph',
-        'birthLocation': 'London, England'
-    },
-    'chance the rapper':{
-        'age': 29,
-        'birthName': 'Chancelor Bennett',
-        'birthLocation': 'Chicago, Illinois' 
-    },
+const boba = {
+    'taro boba': {
+        'prep-time': 3,
+        'pearl colour': 'Purple',
+        'flavour': 'Taro'},
+
+    'brown sugar boba':{
+        'prep-time': 5,
+        'pearl colour': 'Black',
+        'flavour': 'Brown Sugar' },
+
     'unknown':{
-        'age': 0,
-        'birthName': 'unknown',
-        'birthLocation': 'unknown'
-    }
-}
-app.get('/', (request, response)=>{
-    response.sendFile(__dirname + '/index.html')
-})
+        'prep-time': 0,
+        'pearl colour': 'unknown',
+        'flavour': 'unknown'}}
+
+app.get('/',(request, response)=>{
+    response.sendFile(__dirname + '/index.html')})
 
 app.get('/api/:name',(request,response)=>{
-    const rapperName = request.params.name.toLowerCase()
+    const bobaName = request.params.name.toLowerCase()
 
-    if( rappers[rapperName] ){
-        response.json(rappers[rapperName])
-    }else{
-        response.json(rappers['unknown'])
-    }
-    
-})
+    if ( boba[bobaName] ){
+        response.json(boba[bobaName])}
+    else {
+        response.json(boba['unknown'])}})
 
 app.listen(process.env.PORT || PORT, ()=>{
-    console.log(`The server is now running on port ${PORT}! Betta Go Catch It!`)
-})
+    console.log(`The server is now running on port ${PORT}!`)})
